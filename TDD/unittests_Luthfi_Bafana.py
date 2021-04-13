4b401ebb71ed69dea9621c54afe481ed9e3b8c0a
luthfibafana <luthfi.bin-fauzi-bafana@ensta-paris.fr>
2021-04-13 17:45:16 +0200

    unittests for luthfi

---
+++ TDD/unittests_Luthfi_Bafana.py
@@ -0,0 +1,31 @@
+import unittest
+
+from something import subshape.py
+
+class TestCircle(unittest.TestCase):
+
+    def test_get_area_circle(self):
+        c = Circle(1,1,r,g,b,1)
+        area = c.get_area()
+        self.assertEqual(area, 3.14)
+    
+
+    def test_get_perimeter_circle(self):
+        c = Circle(1,1,r,g,b,1)
+        perimeter = c.get_perimeter()
+        self.assertEqual(perimeter, 6.28)
+
+class TestRect(unittest.TestCase):
+
+    def test_get_area_rect(self):
+        r = Rect(1,1,r,g,b,1,2,5)
+        area = r.get_area()
+        self.assertEqual(area, 10)
+
+    def test_get_perimeter_rect(self):
+        r = Rect(1,1,r,g,b,1,2,5)
+        perimeter = r.get_perimeter()
+        self.assertEqual(perimeter, 14)
+
+if __name__ == '__main__':
+    unittest.main()
