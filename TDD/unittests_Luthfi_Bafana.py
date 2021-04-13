c5be0a6a4c28f63d8a6f6234749da32814c8248b
luthfibafana <luthfi.bin-fauzi-bafana@ensta-paris.fr>
2021-04-13 18:32:18 +0200

    unittest for luthfi

--- TDD/unittests_Luthfi_Bafana.py
+++ TDD/unittests_Luthfi_Bafana.py
@@ -1,31 +1,31 @@
 import unittest
-
+import shape
 import subshape
 
 class TestCircle(unittest.TestCase):
 
     def test_get_area_circle(self):
-        c = Circle(1,1,r,g,b,1)
+        c = subshape.Circle()
         area = c.get_area()
         self.assertEqual(area, 3.14)
     
 
     def test_get_perimeter_circle(self):
-        c = Circle(1,1,r,g,b,1)
+        c = subshape.Circle()
         perimeter = c.get_perimeter()
-        self.assertEqual(perimeter, 6.28)
+        self.assertEqual(perimeter, -6.28)
 
 class TestRect(unittest.TestCase):
 
     def test_get_area_rect(self):
-        r = Rect(1,1,r,g,b,1,2,5)
+        r = subshape.Rect()
         area = r.get_area()
-        self.assertEqual(area, 10)
+        self.assertEqual(area, 0)
 
     def test_get_perimeter_rect(self):
-        r = Rect(1,1,r,g,b,1,2,5)
+        r = subshape.Rect()
         perimeter = r.get_perimeter()
-        self.assertEqual(perimeter, 14)
+        self.assertEqual(perimeter, 0)
 
 if __name__ == '__main__':
     unittest.main()
